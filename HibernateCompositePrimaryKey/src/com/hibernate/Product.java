44d1d3b5758a1e72fe192c85ef9d7a8c2854b854
venkataswamy1027 <ecevenkataswamy@gmail.com>
2017-02-05 14:47:45 +0530

    hibernte exmples

---
+++ HibernateCompositePrimaryKey/src/com/hibernate/Product.java
@@ -0,0 +1,36 @@
+package com.hibernate;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+
+
+	private static final long serialVersionUID = 1L;
+	
+	private int productId;
+	private String proName;
+	private double price;
+	
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProName() {
+		return proName;
+	}
+	public void setProName(String proName) {
+		this.proName = proName;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+ 
+	
+	
+}
